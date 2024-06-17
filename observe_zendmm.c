4a226e59e3b262c08a666077af57ded5b4ba7cce
Florian Engelhardt <flo@dotbox.org>
2024-06-17 10:49:16 +0200

    fix to reflect latest upstream pr

--- observe_zendmm.c
+++ observe_zendmm.c
@@ -9,64 +9,59 @@
 #include <stddef.h>
 #include <stdio.h>
 
-// the previous heap that was found in ZendMM
-zend_mm_heap* prev_heap = NULL;
+ZEND_DECLARE_MODULE_GLOBALS(observe_zendmm)
 
-// the custom handlers that might have been found in the previous heap in case
-// there are any
-void* (*custom_malloc)(size_t) = NULL;
-void (*custom_free)(void*) =  NULL;
-void* (*custom_realloc)(void *, size_t) = NULL;
-size_t (*custom_gc)(void) = NULL;
-void (*custom_shutdown)(bool, bool) = NULL;
-
-// this is our heap that we install our custom handlers on and inject into
-// ZendMM
-zend_mm_heap* heap = NULL;
-
-void* observe_malloc(size_t size)
+void* observe_malloc(size_t size ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC)
 {
 	void *ptr = NULL;
-	if (custom_malloc) {
-		ptr = custom_malloc(size);
+	if (OZ_G(custom_malloc)) {
+		zend_mm_set_heap(OZ_G(original_heap));
+		ptr = OZ_G(custom_malloc)(size ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
+		zend_mm_set_heap(OZ_G(observed_heap));
 	} else {
-		ptr = _zend_mm_alloc(prev_heap, size);
+		ptr = _zend_mm_alloc(OZ_G(original_heap), size ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
 	}
 	printf("Allocated %zu bytes at %p\n", size, ptr);
 	return ptr;
 }
 
-void observe_free(void* ptr)
+void observe_free(void* ptr ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC)
 {
-	if (custom_free)
+	if (OZ_G(custom_free))
 	{
-		custom_free(ptr);
+		zend_mm_set_heap(OZ_G(original_heap));
+		OZ_G(custom_free)(ptr ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
+		zend_mm_set_heap(OZ_G(observed_heap));
 	} else {
-		_zend_mm_free(prev_heap, ptr);
+		_zend_mm_free(OZ_G(original_heap), ptr ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
 	}
 	printf("Freed memory at %p\n", ptr);
 }
 
-void* observe_realloc(void* ptr, size_t size)
+void* observe_realloc(void* ptr, size_t size ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC)
 {
 	void * new_ptr;
-	if (custom_realloc)
+	if (OZ_G(custom_realloc))
 	{
-		new_ptr = custom_realloc(ptr, size);
+		zend_mm_set_heap(OZ_G(original_heap));
+		new_ptr = OZ_G(custom_realloc)(ptr, size ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
+		zend_mm_set_heap(OZ_G(observed_heap));
 	} else {
-		new_ptr = _zend_mm_realloc(prev_heap, ptr, size);
+		new_ptr = _zend_mm_realloc(OZ_G(original_heap), ptr, size ZEND_FILE_LINE_RELAY_CC ZEND_FILE_LINE_ORIG_RELAY_CC);
 	}
 	printf("Realloc of %zu bytes from %p to %p\n", size, ptr, new_ptr);
 	return new_ptr;
 }
 
-size_t observe_gc()
+size_t observe_gc(void)
 {
 	size_t size = 0;
-	if (custom_gc) {
-		size = custom_gc();
+	if (OZ_G(custom_gc)) {
+		zend_mm_set_heap(OZ_G(original_heap));
+		size = OZ_G(custom_gc)();
+		zend_mm_set_heap(OZ_G(observed_heap));
 	} else {
-		size = zend_mm_gc(prev_heap);
+		size = zend_mm_gc(OZ_G(original_heap));
 	}
 	printf("ZendMM GC freed %zu bytes", size);
 	return size;
@@ -74,62 +69,110 @@ size_t observe_gc()
 
 void observe_shutdown(bool full, bool silent)
 {
-	if (custom_shutdown) {
-		custom_shutdown(full, silent);
+	if (OZ_G(custom_shutdown)) {
+		zend_mm_set_heap(OZ_G(original_heap));
+		OZ_G(custom_shutdown)(full, silent);
+		zend_mm_set_heap(OZ_G(observed_heap));
 	} else {
-		zend_mm_shutdown(prev_heap, full, silent);
+		zend_mm_shutdown(OZ_G(original_heap), full, silent);
 	}
 	printf("Shutdown happened: full -> %d, silent -> %d\n", full, silent);
 }
 
-PHP_RINIT_FUNCTION(observe_zendmm)
+void observe_zendmm_custom_handlers_init(void)
 {
-	prev_heap = zend_mm_get_heap();
-	if (zend_mm_is_custom_heap(prev_heap)) {
-		zend_mm_get_custom_handlers_ex(
-			prev_heap,
-			&custom_malloc,
-			&custom_free,
-			&custom_realloc,
-			&custom_gc,
-			&custom_shutdown
+	if (OZ_G(observed_heap) != NULL) {
+		return;
+	}
+	OZ_G(original_heap) = zend_mm_get_heap();
+	if (zend_mm_is_custom_heap(OZ_G(original_heap))) {
+		zend_mm_get_custom_handlers(
+			OZ_G(original_heap),
+			&OZ_G(custom_malloc),
+			&OZ_G(custom_free),
+			&OZ_G(custom_realloc),
+			&OZ_G(custom_gc),
+			&OZ_G(custom_shutdown)
 		);
 	}
-	printf("Prev handlers at %p, %p, %p, %p, %p\n", custom_malloc, custom_free, custom_realloc, custom_gc, custom_shutdown);
-	heap = zend_mm_heap_create();
-	zend_mm_set_custom_handlers_ex(
-		heap,
+	printf("Prev handlers at %p, %p, %p, %p, %p\n", OZ_G(custom_malloc), OZ_G(custom_free), OZ_G(custom_realloc), OZ_G(custom_gc), OZ_G(custom_shutdown));
+	OZ_G(observed_heap) = zend_mm_startup();
+	zend_mm_set_custom_handlers(
+		OZ_G(observed_heap),
 		observe_malloc,
 		observe_free,
 		observe_realloc,
 		observe_gc,
 		observe_shutdown
 	);
-	zend_mm_set_heap(heap);
-	printf("Heap at %p installed in ZendMM (orig at %p)\n", heap, prev_heap);
+	zend_mm_set_heap(OZ_G(observed_heap));
+	printf("Heap at %p installed in ZendMM (orig at %p)\n", OZ_G(observed_heap), OZ_G(original_heap));
+}
+
+void observe_zendmm_custom_handlers_shutdown(void)
+{
+	if (OZ_G(observed_heap) == NULL) {
+		return;
+	}
+	zend_mm_set_heap(OZ_G(original_heap));
+	zend_mm_set_custom_handlers(
+		OZ_G(observed_heap),
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL
+	);
+	zend_mm_shutdown(OZ_G(observed_heap), true, true);
+	OZ_G(observed_heap) = NULL;
+	printf("Prev heap at %p restored in ZendMM\n", OZ_G(original_heap));
+}
+
+PHP_RINIT_FUNCTION(observe_zendmm)
+{
 	return SUCCESS;
 }
 
 PHP_RSHUTDOWN_FUNCTION(observe_zendmm)
 {
-	zend_mm_set_heap(prev_heap);
-	zend_mm_heap_free(heap);
-	heap = NULL;
-	printf("Prev heap at %p restored in ZendMM\n", prev_heap);
 	return SUCCESS;
+}
+
+PHP_MINIT_FUNCTION(observe_zendmm)
+{
+	return SUCCESS;
+}
+
+PHP_MSHUTDOWN_FUNCTION(observe_zendmm)
+{
+	return SUCCESS;
+}
+
+PHP_GINIT_FUNCTION(observe_zendmm)
+{
+	observe_zendmm_custom_handlers_init();
+}
+
+PHP_GSHUTDOWN_FUNCTION(observe_zendmm)
+{
+	observe_zendmm_custom_handlers_shutdown();
 }
 
 zend_module_entry observe_zendmm_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"observe_zendmm",					/* Extension name */
 	NULL,					/* zend_function_entry */
-	NULL,							/* PHP_MINIT - Module initialization */
-	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
+	PHP_MINIT(observe_zendmm),							/* PHP_MINIT - Module initialization */
+	PHP_MSHUTDOWN(observe_zendmm),							/* PHP_MSHUTDOWN - Module shutdown */
 	PHP_RINIT(observe_zendmm),			/* PHP_RINIT - Request initialization */
 	PHP_RSHUTDOWN(observe_zendmm),							/* PHP_RSHUTDOWN - Request shutdown */
 	NULL,			/* PHP_MINFO - Module info */
 	PHP_OBSERVE_ZENDMM_VERSION,		/* Version */
-	STANDARD_MODULE_PROPERTIES
+	PHP_MODULE_GLOBALS(observe_zendmm),
+	PHP_GINIT(observe_zendmm),
+	PHP_GSHUTDOWN(observe_zendmm),
+	NULL,
+	STANDARD_MODULE_PROPERTIES_EX
 };
 
 #ifdef COMPILE_DL_OBSERVE_ZENDMM
--- php_observe_zendmm.h
+++ php_observe_zendmm.h
@@ -8,8 +8,30 @@ extern zend_module_entry observe_zendmm_module_entry;
 
 # define PHP_OBSERVE_ZENDMM_VERSION "0.1.0"
 
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
 # if defined(ZTS) && defined(COMPILE_DL_OBSERVE_ZENDMM)
 ZEND_TSRMLS_CACHE_EXTERN()
 # endif
+
+ZEND_BEGIN_MODULE_GLOBALS(observe_zendmm)
+	// the previous heap that was found in ZendMM
+	zend_mm_heap* original_heap;
+	// the custom handlers that might have been found in the previous heap
+	void* (*custom_malloc)(size_t ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
+	void (*custom_free)(void* ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
+	void* (*custom_realloc)(void *, size_t ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
+	size_t (*custom_gc)(void);
+	void (*custom_shutdown)(bool, bool);
+	// this is our heap that we install our custom handlers on and inject into
+	// ZendMM
+	zend_mm_heap* observed_heap;
+ZEND_END_MODULE_GLOBALS(observe_zendmm)
+
+extern ZEND_DECLARE_MODULE_GLOBALS(observe_zendmm)
+
+#define OZ_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(observe_zendmm, v)
 
 #endif	/* PHP_OBSERVE_ZENDMM_H */
